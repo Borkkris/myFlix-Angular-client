ba824c15e716fe41e87c218541ef4ed4f960661f
Borkkris <Borkkris@users.noreply.github.com>
2022-11-12 12:28:43 +0100

    Update fetch-api-data.service.ts
    minor changes

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -10,7 +10,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://app-my-flix.herokuapp.com';
+const apiUrl = 'https://app-my-flix.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
