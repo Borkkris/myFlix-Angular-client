9ca7792e53b6d61368d41b38bb8765def2e06da5
Borkkris <Borkkris@users.noreply.github.com>
2022-11-14 14:39:05 +0100

    minor changes

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -29,8 +29,7 @@ export class UserRegistrationFormComponent implements OnInit {
   // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-      // Logic for a successful user registration goes here! (To be implemented)
-      this.dialogRef.close(); // This will close the modal on success!
+      this.dialogRef.close(); // This will close the modal after completion
       console.log(response);
       this.snackBar.open('User registered successfully', 'OK', {
         duration: 2000
